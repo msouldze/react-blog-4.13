36bbe5788f76864829552171e43ade2ff3e7bf91
Aizhan Dzhumanalieva <msouldze1@gmail.com>
2024-06-11 20:42:18 +0900

    adding styles, routes, components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,67 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import ArticlesList from './components/ArticlesList'
+import { useState, lazy, Suspense } from 'react'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements } from 'react-router-dom'
+
+// components
+import HeaderLayout from './layouts/HeaderLayout'
+import Signup from './components/Signup'
+import Signin from './components/Signin'
+import EditProfile from './components/EditProfile'
+import CreateArticle from './components/articles/CreateArticle'
+import EditArticle from './components/articles/EditArticle'
+import Loader from './components/Loader'
+import NotFound from './components/NotFound'
+import ArticlesError from './components/articles/ArticlesError'
+
+// loaders
+import { articlesLoader } from './components/articles/ArticlesList'
+import { articleDataLoader } from './components/articles/Article'
+
+// styles
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ArticlesList = lazy(() => import('./components/articles/ArticlesList'))
+const Article = lazy(() => import('./components/articles/Article'))
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<HeaderLayout />} errorElement={<ArticlesError/>} >
+      <Route
+        index
+        loader={articlesLoader} 
+        element={(
+          <Suspense fallback={<Loader />}>
+            <ArticlesList/>
+          </Suspense>
+        )}
+      />
+      <Route
+        path=':slug'
+        loader={articleDataLoader}
+        element={(
+          <Suspense fallback={<Loader />}>
+            <Article/>
+          </Suspense>
+        )}/>
+      <Route path='signin' element={<Signin />} />
+      <Route path='signup' element={<Signup />} />
+      <Route path='new-article' element={<CreateArticle />} />
+      <Route path='edit-article' element={<EditArticle />} />
+      <Route path='edit-profile' element={<EditProfile />} />
+
+      <Route path='*' element={<NotFound />} />
+    </Route>
+  )
+);
+
+function App() {
   return (
-    <>
-      <Header />
-      <ArticlesList />
-    </>
+    <RouterProvider router={router} />
   )
 }
+
 
 export default App
